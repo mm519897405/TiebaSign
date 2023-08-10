6315678b81f79ad9bf27e1c4efad7095006ea87f
mm519897405 <baiya@vip.qq.com>
2023-08-10 17:31:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def send_email(sign_list):
     msg = MIMEText(body, 'html', 'utf-8')
     msg['subject'] = subject
     smtp = smtplib.SMTP()
-    print(HOST)
+    logger.error("HOST=%s",HOST)
     smtp.connect(HOST)
     smtp.login(FROM, AUTH)
     smtp.sendmail(FROM, TO, msg.as_string())
